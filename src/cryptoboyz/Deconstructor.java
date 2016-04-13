b87c61fb0a25e1b3bb953c930057104d43a2c5c1
Ben <ben@stolovitz.com>
2016-04-13 16:25:20 +0300

    hella formatting

--- src/cryptoboyz/Deconstructor.java
+++ src/cryptoboyz/Deconstructor.java
@@ -1,67 +1,78 @@
 package cryptoboyz;
 
 import java.math.BigInteger;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Random;
 
 public class Deconstructor {
-	
+
 	private BigInteger prime;
 	private Polynomial polynomial;
 	private int sharesGenerated;
-	
+
 	/**
-	 * Create a Deconstructor to spread the word to your friends! 
+	 * Create a Deconstructor to spread the word to your friends!
+	 * 
 	 * @param message the message to send
-	 * @param k the minimum number of shares required to reconstruct the polynomial
+	 * @param k the minimum number of shares required to reconstruct the
+	 *            polynomial
 	 */
 	public Deconstructor(String message, int k) {
-		// TODO check p > n (size of field should be larger than number of participants)
-		
+		// TODO check p > n (size of field should be larger than number of
+		// participants)
+
 		Random rnd = new Random();
 		prime = BigInteger.probablePrime(1000, rnd);
 		
-		//after generating p, convert message to integer value, this will be the constant term of the polynomial
+		// after generating p, convert message to integer value, this will be
+		// the constant term of the polynomial
 		// TODO generate a polynomial for our message
-		BigInteger msgtoint = BigInteger.valueOf(4815); //FIXME
+		BigInteger msgtoint = BigInteger.valueOf(4815); // FIXME
 		Polynomial poly = new Polynomial(msgtoint, 0);
 
-		//polynomial of degree k-1
-		for(int i=1; i<k; i++){
+		// polynomial of degree k-1
+		for (int i = 1; i < k; i++) {
 			BigInteger coeff = new BigInteger(1000, rnd);
-			while(coeff.compareTo(prime) > 0){
+			while (coeff.compareTo(prime) > 0) {
 				coeff = new BigInteger(1000, rnd);
 			}
 			Polynomial term = new Polynomial(coeff, i);
 			poly.plus(term);
 		}
 		this.polynomial = poly;
-		 
+
 	}
-	
-	public Share generate(){
+
+	public Share generate() {
 		int x = this.sharesGenerated;
-		BigInteger y = polynomial.evaluate(x).mod(this.prime); //each share is (x, f(x) mod p)
+		BigInteger y = polynomial.evaluate(x).mod(this.prime); // each share is
+																// (x, f(x) mod
+																// p)
+		
+		System.out.println(polynomial.evaluate(100));
+		
 		this.sharesGenerated += 1;
 		return new Share(x, y);
 	}
-	
+
 	public HashSet<Share> generate(int n) {
 		HashSet<Share> shares = new HashSet<Share>();
-		for(int x=1; x<n; x++){
+		for (int x = 1; x < n; x++) {
 			shares.add(this.generate());
 		}
 		return shares;
 	}
-	
+
+	public BigInteger getPrime() {
+		return this.prime;
+	}
+
 	public static void main(String[] args) {
 		// Parse arguments here, TODO
 		Deconstructor s = new Deconstructor("message", 8);
-		
+
 		int m = 10;
-		for(int i = 0; i < m; i++) {
+		for (int i = 0; i < m; i++) {
 			System.out.println(s.generate());
 		}
 	}
--- src/cryptoboyz/Reconstructor.java
+++ src/cryptoboyz/Reconstructor.java
@@ -5,44 +5,39 @@ import java.util.HashSet;
 
 public class Reconstructor {
 	protected String message;
-	private BigInteger p;
-	
-	public Reconstructor(HashSet<Share> shares, BigInteger p) {
-		this(shares, shares.size(), p);
+
+	public Reconstructor(HashSet<Share> shares, BigInteger prime) {
+		this(shares, shares.size(), prime);
 	}
-	
+
 	private Reconstructor(HashSet<Share> shares, int k, BigInteger p) {
-		// TODO lagrangian stuff
-		// TODO solve
 		BigInteger sum = BigInteger.ZERO;
-		for(Share i: shares){
-			BigInteger f = i.getY(); //f(x_i)
+		for (Share i : shares) {
+			BigInteger f = i.getY(); // f(x_i)
 			BigInteger m = BigInteger.ONE;
-			for (Share j: shares){
-				if(!j.equals(i)){
+			for (Share j : shares) {
+				if (!j.equals(i)) {
 					int xj = j.getX();
 					int xi = i.getX();
 					BigInteger sub = BigInteger.valueOf(xj - xi);
 					BigInteger inv = Util.inverse(sub, p);
-					
+
 					BigInteger num = BigInteger.valueOf(xj);
 					m = m.multiply(num.multiply(inv));
-					
+
 				}
 			}
 			sum.add(f.multiply(m));
 		}
 	}
-	
-	
-	
+
 	public String getMessage() {
 		return this.message;
 	}
-	
+
 	public static void main(String[] args) {
 		// TODO actually give our reconstructor a list of shares.
-		Reconstructor r = new Reconstructor(null);
+		Reconstructor r = new Reconstructor(null, null);
 		System.out.println(r.getMessage());
 	}
 
