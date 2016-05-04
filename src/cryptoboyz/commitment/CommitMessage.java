2168bb11ed37d0a2abb88ee3fa117a06c17826b7
frozenberg <fede.rozenberg@gmail.com>
2016-05-04 18:37:35 +0300

    my bad

--- src/cryptoboyz/commitment/CommitMessage.java
+++ src/cryptoboyz/commitment/CommitMessage.java
@@ -8,58 +8,12 @@ public class CommitMessage {
 	private GroupNumber alpha;
 	private GroupNumber g;
 	
-	/**
-	 * Do you have information that you want to prove that you have, without sharing that
-	 * information with any of those other losers (I'm looking at you, Bob)? Are you tired
-	 * of people insinuating that you are choosing bits in a way that secretly gets you 
-	 * information about DLOG?
-	 * 
-	 * Boy do we have an answer for your prayers.
-	 * 
-	 * Generate a CommitMessage to share with the world! Commit to values and 
-	 * make people believe that you have!
-	 * 
-	 * First, you're gonna need a group generator g, and you're gonna want to ask the other
-	 * party to generate some alpha g^u (YOU don't get to know "u", get it?).
-	 * 
-	 * Then you need some information. Store it in a GroupNumber. Pass it as an argument.
-	 * 
-	 * @param g the magical generator
-	 * @param alpha your untrusting "friend"'s random number
-	 * @param info your hidden information
-	 * @return a CommitMessage that you can share with anybody!
-	 */
-	public static CommitMessage Generate(GroupNumber g, GroupNumber alpha, GroupNumber info) {
-		GroupNumber u = g.getGroup().generateMember();
-		GroupNumber commitment = alpha.exp(info).multiply(g.exp(u));
-		return new CommitMessage(g, alpha, commitment);
-	}
-	
-	/**
-	 * A CommitMessage is the sharable result of a commitment :)
-	 * 
-	 * It can be decommited if given the "commit" result and a key. See the decommit function.
-	 * 
-	 * @param g the generator used in our amazing Commit sequence
-	 * @param alpha the verifier-generated random number used at the beginning of the sequence
-	 * @param commitment the encrypted value---our commitment.
-	 */
-	protected CommitMessage(GroupNumber g, GroupNumber alpha, GroupNumber commitment) {
-		this.committed = commitment;
+	public CommitMessage(GroupNumber committed, GroupNumber alpha, GroupNumber g) {
+		this.committed = committed;
 		this.alpha = alpha;
 		this.g = g;
 	}
 	
-	/**
-	 * De-commit a CommitMessage into its original value.
-	 * 
-	 * Throws a TrustException if what you de-committed to was not correct.
-	 * 
-	 * @param info what Alice says she committed too
-	 * @param c what Alice says she used to generate her commitment
-	 * @return what Alice committed too, unless she was lying
-	 * @throws TrustException
-	 */
 	public GroupNumber decommit(GroupNumber info, GroupNumber c) throws TrustException {
 		if(!this.alpha.exp(info).multiply(this.g.exp(c)).equals(committed)) {
 			throw new TrustException("Committed value does not match sent information " + info);
