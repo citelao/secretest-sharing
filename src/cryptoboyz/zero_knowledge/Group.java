ee46331457fb423722270fa164a1c729fe2776ad
frozenberg <fede.rozenberg@gmail.com>
2016-05-04 13:44:20 +0300

    groups and numbers

--- src/cryptoboyz/zero_knowledge/Group.java
+++ src/cryptoboyz/zero_knowledge/Group.java
@@ -1,5 +1,27 @@
 package cryptoboyz.zero_knowledge;
 
-public class Group {
+import java.math.BigInteger;
+import java.util.Random;
 
+public class Group {
+	
+	private final BigInteger order;
+	
+	public Group(){
+		Random rnd = new Random();
+		this.order = BigInteger.probablePrime(1000, rnd);
+	}
+	
+	public GroupNumber generateMember(){
+		Random rnd = new Random();
+		BigInteger number = new BigInteger(1000, rnd);
+		while (number.compareTo(order) > 0) {
+			number = new BigInteger(1000, rnd);
+		}
+		return new GroupNumber(number, order);
+	}
+	
+	public BigInteger getOrder(){
+		return this.order;
+	}
 }
--- src/cryptoboyz/zero_knowledge/GroupNumber.java
+++ src/cryptoboyz/zero_knowledge/GroupNumber.java
@@ -1,5 +1,24 @@
 package cryptoboyz.zero_knowledge;
 
-public class GroupNumber {
+import java.math.BigInteger;
 
+public class GroupNumber {
+	
+	private BigInteger value;
+	private BigInteger order;
+	
+	public GroupNumber(BigInteger n, BigInteger q){
+		this.value = n;
+		this.order = q;
+	}
+	
+	public GroupNumber inverse(){
+		return new GroupNumber(this.value.modInverse(order), order);
+	}
+	
+	public GroupNumber exp(GroupNumber k){
+		return new GroupNumber(this.value.modPow(k.value, order), order);
+	}
+	
+	
 }
